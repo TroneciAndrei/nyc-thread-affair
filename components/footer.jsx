73031a399a32508c6585c8c9268ee8617a492962
Troneci Andrei <troneciandrei@gmail.com>
2022-05-08 17:33:11 +0300

    thread affair colors done

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,201 +1,212 @@
 export default function Footer() {
-  return (<footer className="footer">
-    <section className="footer-primary">
-      <div className="footer-primary-inner container">
-        <section className="footer-nav">
-          <ul>
-            <li>
-              <a href="" title="Link">
-                Link text
+  return (
+    <footer className="footer">
+      <section className="footer-primary">
+        <div className="footer-primary-inner container">
+          <section className="footer-nav">
+            <ul>
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
-          </ul>
+              </li>
+            </ul>
 
-          <ul>
-            <li>
-              <a href="" title="Link">
-                Link text
+            <ul>
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
-          </ul>
+              </li>
+            </ul>
 
-          <ul>
-            <li>
-              <a href="" title="Link">
-                Link text
+            <ul>
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
-          </ul>
+              </li>
+            </ul>
 
-          <ul>
-            <li>
-              <a href="" title="Link">
-                Link text
+            <ul>
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
+              </li>
 
-            <li>
-              <a href="" title="Link">
-                Link text
+              <li>
+                <a href="" title="Link">
+                  Link text
                 </a>
-            </li>
-          </ul>
-        </section>
+              </li>
+            </ul>
+          </section>
 
-        <section className="footer-support">
-          <ul>
-            <li>
-              <a href="" title="Support" className="text-uppercase">
-                <i className="fa-solid fa-phone"></i>
-                Support
-              </a>
-            </li>
+          <section className="footer-support">
+            <ul>
+              <li>
+                <a href="" title="Support" className="text-uppercase">
+                  <i className="fa-solid fa-phone"></i>
+                  Support
+                </a>
+              </li>
 
-            <li>
-              <a href="" title="Support" className="text-uppercase">
-                <i className="fa-solid fa-phone"></i>
-                Support
-              </a>
-            </li>
+              <li>
+                <a href="" title="Support" className="text-uppercase">
+                  <i className="fa-solid fa-phone"></i>
+                  Support
+                </a>
+              </li>
 
-            <li>
-              <a href="" title="Support" className="text-uppercase">
-                <i className="fa-solid fa-phone"></i>
-                Support
-              </a>
-            </li>
-          </ul>
-        </section>
+              <li>
+                <a href="" title="Support" className="text-uppercase">
+                  <i className="fa-solid fa-phone"></i>
+                  Support
+                </a>
+              </li>
+            </ul>
+          </section>
 
-        <section className="footer-contact">
-          <p>
-          Largo Augusto 3, Milano, 20122, Milano, IT Tel: 0039 02 760 003 66 office@thread-affair.com
-          </p>
-        </section>
-      </div>
-    </section>
+          <section className="footer-contact">
+            <h1>
+              <span>Thread Affair</span> Milano
+            </h1>
+            <p>
+              Largo Augusto 3, Milano, 20122, Milano, IT Tel: 0039 02 760 003 66
+              <br />
+              <a href="mailto:">office@thread-affair.com</a>
+            </p>
+          </section>
+        </div>
+      </section>
 
-    <section className="footer-secondary text-center">
-      <p className="mb-2">©2019 THREAD AFFAIR. All Rights Reserved.</p>
+      <section className="footer-secondary text-center">
+        <p className="mb-2">©2019 THREAD AFFAIR. All Rights Reserved.</p>
 
-      <p className="d-flex align-items-center justify-content-center">
-        Design by
-        <img className="ms-2" src="/images/pixellab_logo.jpg" alt="Pixellab" />
-      </p>
-    </section>
-  </footer>);
+        <p className="d-flex align-items-center justify-content-center">
+          Design by
+          <img
+            className="ms-2"
+            src="/images/pixellab_logo.jpg"
+            alt="Pixellab"
+          />
+        </p>
+      </section>
+    </footer>
+  );
 }
