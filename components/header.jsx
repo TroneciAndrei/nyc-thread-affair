b863be190abe91404142486937c586f895c2398f
Troneci Andrei <troneciandrei@gmail.com>
2022-05-11 09:08:21 +0300

    Thread Affair main start

--- components/header.jsx
+++ components/header.jsx
@@ -1,121 +1,121 @@
 export default function Header() {
   return (
-  <header className="header container mt-0 mt-lg-4">
-    <section className="header-secondary">
-      <div className="header-connect  d-none d-lg-block">
-        <ul>
-          <li>
-            <a href="tel:+" title="Call us">
-            <i className="fa-solid fa-phone"></i>
-            Toll Free 1-877-666-1840
-            </a>
-          </li>
-
-          <li>
-            <a href="mailto:" title="Mail us">
-              office@traidaffairs.com
+    <header className="header container mt-0 mt-lg-4">
+      <section className="header-secondary">
+        <div className="header-connect  d-none d-lg-block">
+          <ul>
+            <li>
+              <a href="tel:+" title="Call us">
+                <i className="fa-solid fa-phone"></i>
+                Toll Free 1-877-666-1840
               </a>
-          </li>
-        </ul>
-      </div>
+            </li>
 
-      <div className="header-search d-lg-none">
-        <ul>
-          <li>
-            <a href="" title="Menu">
-            <i class="fa-solid fa-bars"></i>
+            <li>
+              <a href="mailto:" title="Mail us">
+                office@traidaffairs.com
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="header-search d-lg-none">
+          <ul>
+            <li>
+              <a href="" title="Menu">
+                <i className="fa-solid fa-bars"></i>
+              </a>
+            </li>
+
+            <li>
+              <a href="" title="Search">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="header-controls">
+          <section className="user-controls">
+            <a href="" title="Account">
+              <img src="/images/user-avatar.png" alt="User" />
+
+              <i className="fa-solid fa-angle-down"></i>
             </a>
-          </li>
+          </section>
 
-          <li>
-            <a href="" title="Search">
-            <i class="fa-solid fa-magnifying-glass"></i>
+          <section className="cart-controls">
+            <a href="" title="Cart">
+              <i className="fas fa-shopping-cart"></i>
+              <span className="cart-qty">3</span>
             </a>
-          </li>
-        </ul>
-      </div>
+          </section>
+        </div>
 
-      <div className="header-controls">
-        <section className="user-controls">
-          <a href="" title="Account">
-            <img src="/images/user-avatar.png" alt="User" />
-
-          <i className="fa-solid fa-angle-down"></i>
+        <div className="header-logo text-center">
+          <a href="https://google.com">
+            <img src="/images/logo.jpg" alt="Thread Affair - Logo" />
           </a>
-        </section>
+        </div>
+      </section>
 
-        <section className="cart-controls">
-          <a href="" title="Cart">
-          <i className="fas fa-shopping-cart"></i>
-          <span className="cart-qty">3</span>
-          </a>
-        </section>
-      </div>
-
-      <div className="header-logo text-center">
-        <a href="https://google.com">
-        <img src="/images/logo.jpg" alt="Thread Affair - Logo" />
-        </a>
-      </div>
-    </section>
-
-    <section className="header-primary d-none d-lg-block">
-      <nav className="nav-primary">
-        <ul>
-          <li>
-            <a href="" title="Costume">
-              Costume
+      <section className="header-primary d-none d-lg-block">
+        <nav className="nav-primary">
+          <ul>
+            <li>
+              <a href="" title="Costume">
+                Costume
               </a>
-          </li>
+            </li>
 
-          <li>
-            <a href="" title="Costume">
-              Costume
+            <li>
+              <a href="" title="Costume">
+                Costume
               </a>
-          </li>
+            </li>
 
-          <li>
-            <a href="" title="Costume">
-              Costume
+            <li>
+              <a href="" title="Costume">
+                Costume
               </a>
-          </li>
+            </li>
 
-          <li>
-            <a href="" title="Costume">
-              Costume
+            <li>
+              <a href="" title="Costume">
+                Costume
               </a>
-          </li>
-        </ul>
-      </nav>
+            </li>
+          </ul>
+        </nav>
 
-      <nav className="nav-secondary">
-        <ul>
-          <li>
-            <a href="" title="Office suits">
-              Office suits
+        <nav className="nav-secondary">
+          <ul>
+            <li>
+              <a href="" title="Office suits">
+                Office suits
               </a>
-          </li>
+            </li>
 
-          <li>
-            <a href="" title="Office suits">
-              Office suits
+            <li>
+              <a href="" title="Office suits">
+                Office suits
               </a>
-          </li>
+            </li>
 
-          <li>
-            <a href="" title="Office suits">
-              Office suits
+            <li>
+              <a href="" title="Office suits">
+                Office suits
               </a>
-          </li>
+            </li>
 
-          <li>
-            <a href="" title="Office suits">
-              Office suits
+            <li>
+              <a href="" title="Office suits">
+                Office suits
               </a>
-          </li>
-        </ul>
-      </nav>
-    </section>
-  </header>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </header>
   );
 }
